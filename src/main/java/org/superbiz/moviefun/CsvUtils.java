9e5d415ef4386b92818c411a0c7cef905f13597e
Chris <cwegner2@ford.com>
2019-04-26 12:14:04 +0100

    Read from files in Classpath to insert into database

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -13,8 +13,10 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
+        ClassLoader classLoader = CsvUtils.class.getClassLoader();
+
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,8 +24,8 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Could not find " + path + " in classpath");
         }
     }
 
